3ffcda84055894a2fcdefc7e0bf221db89687861
Erk L <ericklo1094011@gmail.com>
2019-02-27 01:54:19 -0500

    I used creat-react-app to configure most of the React app

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+// src/App.js file will serve as main container component
+
+import React, { Component } from 'react';
+import './styles/global.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
